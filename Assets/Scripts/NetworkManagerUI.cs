52749cac7a0532d67f5bfb40a1df2853f46ef2d8
shiro-kreishi <vadas25@yandex.ru>
2024-04-30 18:03:10 +0500

    add player hud

--- Assets/Scripts/NetworkManagerUI.cs
+++ Assets/Scripts/NetworkManagerUI.cs
@@ -1,3 +1,6 @@
+using System;
+using Core.Singletons;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using Unity.Netcode;
@@ -7,6 +10,7 @@ public class NetworkManagerUI : MonoBehaviour
     [SerializeField] private Button serverBtn;
     [SerializeField] private Button hostBtn;
     [SerializeField] private Button clientBtn;
+    [SerializeField] private TextMeshProUGUI playerInGame;
 
     private void Awake()
     {
@@ -22,5 +26,10 @@ public class NetworkManagerUI : MonoBehaviour
         {
             NetworkManager.Singleton.StartClient();
         }));
+    }
+
+    private void Update()
+    {
+        playerInGame.text = $"Player in game: {PlayersManager.Instance.PlayersInGame}";
     }
 }
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Core.Singletons
+{
+    public class PlayersManager : NetworkSingleton<PlayersManager>
+    {
+        private NetworkVariable<int> playersInGame = new NetworkVariable<int>();
+
+        public int PlayersInGame
+        {
+            get
+            {
+                return playersInGame.Value;
+            }
+        }
+
+        private void Start()
+        {
+            NetworkManager.Singleton.OnClientConnectedCallback += (id) =>
+            {
+                if (IsServer)
+                {
+                    Debug.Log($"{id} just connected...");
+                    playersInGame.Value++;
+                }
+            };
+            NetworkManager.Singleton.OnClientDisconnectCallback += (id) =>
+            {
+                if (IsServer)
+                {
+                    Debug.Log($"{id} just disconnected...");
+                    playersInGame.Value--;
+                }
+            };
+        }
+    }
+}
